73c175453ec1509b94c7c31a8908bb43d4eaec43
Eroshina.V.M <VeraEro@yandex.ru>
2026-04-27 13:22:10 +0300

    Верстка окна список машин

---
+++ CarsharingApp/CarsharingApp/Cars.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarsharingApp
+{
+    /// <summary>
+    /// Interaction logic for Cars.xaml
+    /// </summary>
+    public partial class Cars : Window
+    {
+        public Cars()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
